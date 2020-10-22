0141d9b470400a8a1c5d142c5da6fa5283332836
khanh_hoa <your_email@youremail.com>
2020-10-22 15:04:27 +0700

    sua lai

--- hello.java
+++ hello.java
@@ -1,12 +1,10 @@
+import java.util.Scanner;
+
 public class Hello{
-
-public static void main(String[] args){
-	for(int i=1;i<=10;i++)
-	{
-		System.out.println("Hello World..."+i);
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("Hello " + name);
 	}
-	
-}
-
-
 }
